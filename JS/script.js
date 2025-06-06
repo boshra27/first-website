de14e0fa67e063997af7720e5d014d835f859505
Boshra <215034432+boshra27@users.noreply.github.com>
2025-06-06 18:27:39 +0300

    Initial commit – add first website project
    Added HTML, CSS, JS, and image folders for my university course project.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function showSelect(){
+    document.getElementById("where-to").style.display = "block";
+    document.querySelector("button").style.display = "none";
+}
+
+function showGuide(){
+    document.getElementById("travel-guide").style.display = "block";
+}
